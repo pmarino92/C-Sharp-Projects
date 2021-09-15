10679f282b41dfedcdabb223df1e977a6bddcae7
pmarino92 <86321304+pmarino92@users.noreply.github.com>
2021-09-15 13:45:09 -0700

    HelloWorld App

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Aceme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" for you!");
+            Console.ReadLine();
+        }
+    }
+
