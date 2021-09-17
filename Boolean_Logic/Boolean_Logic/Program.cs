15a846ff9e6fa4689e559290e0e1922c1539da3d
pmarino92 <86321304+pmarino92@users.noreply.github.com>
2021-09-17 14:50:50 -0700

    Boolean Logic Assignment

---
+++ Boolean_Logic/Boolean_Logic/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Logic
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Ask what user age is
+            // Store that number in userAge
+            Console.WriteLine("What is your age?");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            // Ask if user has had a DUI
+            // User response is a string
+            Console.WriteLine("Have you ever had a DUI?");
+            bool userDUI = false;
+            Console.ReadLine();
+
+            // Asking user how many speeding tickets they have
+            // Response will be an integer
+            Console.WriteLine("How many speeding tickets do you have?");
+            int userTicket = Convert.ToInt32(Console.ReadLine());
+
+           
+            Console.WriteLine("Qualified?");
+            bool qualified = false;
+            qualified = (userAge > 15 && userDUI == true && userTicket <= 3);
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+
+        }
+    }
+}
