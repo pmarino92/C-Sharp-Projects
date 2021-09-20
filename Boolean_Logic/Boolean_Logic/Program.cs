40e11e23febbf05ec1554a3197acd37e212c0e2c
pmarino92 <86321304+pmarino92@users.noreply.github.com>
2021-09-20 08:00:36 -0700

    Boolean Logic

--- Boolean_Logic/Boolean_Logic/Program.cs
+++ Boolean_Logic/Boolean_Logic/Program.cs
@@ -10,28 +10,28 @@ namespace Boolean_Logic
     {
         static void Main()
         {
-            // Ask what user age is
-            // Store that number in userAge
+            //// Ask what user age is
+            //// Store that number in userAge
             Console.WriteLine("What is your age?");
             int userAge = Convert.ToInt32(Console.ReadLine());
 
-            // Ask if user has had a DUI
-            // User response is a string
+            //// Ask if user has had a DUI
+            //// User response is a string
             Console.WriteLine("Have you ever had a DUI?");
-            bool userDUI = true;
-            Console.ReadLine();
+            string userDUI = Console.ReadLine();
 
-            // Asking user how many speeding tickets they have
-            // Response will be an integer
+            //// Asking user how many speeding tickets they have
+            //// Response will be an integer
             Console.WriteLine("How many speeding tickets do you have?");
             int userTicket = Convert.ToInt32(Console.ReadLine());
 
-           
+
             Console.WriteLine("Qualified?");
-            bool qualified = false;
-            bool isQualified = (userAge > 15 && userDUI == true && userTicket <= 3);
+            bool isQualified = (userAge > 15 && userDUI == "no" && userTicket < 3);
             Console.WriteLine(isQualified);
             Console.ReadLine();
+
+            
 
         }
     }
