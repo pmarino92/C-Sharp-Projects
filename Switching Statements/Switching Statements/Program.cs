407d7a7abf4df259e274cf7f47eb15026da3faf8
pmarino92 <86321304+pmarino92@users.noreply.github.com>
2021-09-20 10:18:13 -0700

    While Loops in C#

---
+++ Switching Statements/Switching Statements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switching_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Starting our number at 0
+            int num = 0;
+            do
+            {
+                ++num;
+                Console.WriteLine("Num is now " + num);
+            }
+
+            // while our number is less than 10
+            // increment our variable "num" by 1
+            while (num < 10);
+         
+            Console.ReadLine();
+        }
+    }
+}
