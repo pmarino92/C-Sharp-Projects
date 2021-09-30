ef9dc2c1003d062782147693a60cd3f11eb3d13e
pmarino92 <philipmarino1@gmail.com>
2021-09-29 18:16:17 -0700

    Optional Parameters

---
+++ Optional Parameters/Optional Parameters/Operator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Parameters
+{
+    public class Operator
+    {
+        public int operate(int data, int input = 0)
+        {
+            return data + input;
+        }
+    }
+}
---
+++ Optional Parameters/Optional Parameters/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Operator operatorObject = new Operator();
+                Console.WriteLine("Pick a number:");
+                int data = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick another number--optional");
+                int input = Convert.ToInt32(Console.ReadLine());
+
+                int result = operatorObject.operate(data, input);
+
+
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("You entered nothing...default is 0");
+                
+             
+                Console.ReadLine();
+            }
+        }
+    }
+}
