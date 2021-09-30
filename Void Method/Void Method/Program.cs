4842258ca1ddd5c6b23985be8a5ec2a6097cee36
pmarino92 <philipmarino1@gmail.com>
2021-09-30 09:26:25 -0700

    Void Method

---
+++ Void Method/Void Method/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subtraction sub = new Subtraction(); //instantiate the class
+
+            //calling the method passing in two numbers
+            sub.Sub(4, 2); 
+
+            // calling the method specifying the parameters by name
+            int a = 4;
+            int b = 2;
+            sub.Sub(a, b);
+            Console.ReadLine();
+            
+        }
+    }
+}
