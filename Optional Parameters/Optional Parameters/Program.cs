47e757c2259885c367834b8dd91d56e02b84080f
pmarino92 <philipmarino1@gmail.com>
2021-09-30 12:26:08 -0700

    Optional Parameters Final

--- Optional Parameters/Optional Parameters/Program.cs
+++ Optional Parameters/Optional Parameters/Program.cs
@@ -10,26 +10,33 @@ namespace Optional_Parameters
     {
         static void Main(string[] args)
         {
+            // Getting first parameter from user
+            Operator operatorObject = new Operator();
+            Console.WriteLine("Enter a number");
+            int data = Convert.ToInt32(Console.ReadLine());
+
             try
             {
-                Operator operatorObject = new Operator();
-                Console.WriteLine("Pick a number:");
-                int data = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Pick another number--optional");
+                // Trying to get second parameter from user
+                Console.WriteLine("Enter another number--optional");
                 int input = Convert.ToInt32(Console.ReadLine());
 
+                // If we get a 2nd parameter form user
+                // Call method with both parameters
                 int result = operatorObject.operate(data, input);
-
 
                 Console.WriteLine(result);
                 Console.ReadLine();
             }
             catch
             {
-                Console.WriteLine("You entered nothing...default is 0");
-                
-             
+                // if the user doesn't enter a 2nd parameter 
+                // run the method with only one parameter
+                // 2nd parameter is optional so the default is 0
+                int result = operatorObject.operate(data);
+                Console.WriteLine(result);
                 Console.ReadLine();
+                
             }
         }
     }
