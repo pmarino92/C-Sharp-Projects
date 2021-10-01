c6c6f6363b6488a0458dee0eaed3e225ef3e20bb
pmarino92 <philipmarino1@gmail.com>
2021-10-01 12:50:43 -0700

    Object Inheritance

---
+++ Methods and Objects/Methods and Objects/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_and_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating employee object
+            Employee employee = new Employee();
+            // Assigning first name
+            employee.FirstName = "Sample";
+            // Assigning last name
+            employee.LastName = "Student";
+
+            // Calling SayName method
+            // Returns Name: SampleStudent to console
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
