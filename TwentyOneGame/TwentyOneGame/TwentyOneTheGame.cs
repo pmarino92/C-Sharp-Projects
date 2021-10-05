0907be9193e7df535c9751a3e86455b3a119933b
pmarino92 <philipmarino1@gmail.com>
2021-10-05 13:11:39 -0700

    Twenty One Game

---
+++ TwentyOneGame/TwentyOneGame/TwentyOneTheGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOneGame
+{
+    public class TwentyOneTheGame : Game, IWalkAway
+    {
+        public override void Play()
+        {
+            throw new NotImplementedException();
+        }
+        public override void ListPlayers()
+        {
+            Console.WriteLine("21 Players:");
+            base.ListPlayers();
+        }
+        public void WalkAway(Player player)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
