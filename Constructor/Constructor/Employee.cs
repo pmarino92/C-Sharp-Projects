dc35f8b6cc08b01f6498a7dca35d161bdb054a2b
pmarino92 <86321304+pmarino92@users.noreply.github.com>
2021-10-07 07:47:51 -0700

    Constructor Assignment

---
+++ Constructor/Constructor/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+   public class Employee
+    {
+        public Employee(string name) : this(name, 1)
+        {
+
+        }
+
+        public Employee(string name, int Id)
+        {
+            Name = name;
+            Id = Id;
+
+        }
+    }
+}
---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating a constant variable
+            // this variable can not change
+            const string myName = "Phil";
+            Console.WriteLine("Hello my name is {0}", myName);
+
+            // Creating a variable using "var"
+            var gfName = "Taylor";
+            Console.WriteLine("My girlfriend's name is {0}", gfName);
+            Console.ReadLine();
+        }
+    }
+}
