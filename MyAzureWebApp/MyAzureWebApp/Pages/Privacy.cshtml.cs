66e15c5d523f5b5f461ce2e95451f8f6af6dd933
pmarino92 <86321304+pmarino92@users.noreply.github.com>
2021-10-14 14:35:46 -0700

    Azure Web App

---
+++ MyAzureWebApp/MyAzureWebApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace MyAzureWebApp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string dateTime = System.DateTime.Now.ToShortDateString();
+            ViewData["TimeStamp"] = dateTime;
+        }
+    }
+}
---
+++ MyAzureWebApp/MyAzureWebApp/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAzureWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            Console.WriteLine(localDate);
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+        
+    }
+
+}
