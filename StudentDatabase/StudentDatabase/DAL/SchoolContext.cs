9f49fbd0e3c137fcb74922d6f467dfd484e728a9
pmarino92 <86321304+pmarino92@users.noreply.github.com>
2021-10-15 10:41:28 -0700

    Student Database

---
+++ StudentDatabase/StudentDatabase/DAL/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using StudentDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace StudentDatabase.DAL
+{
+    public class SchoolContext: DbContext
+    {
+
+        public SchoolContext() : base("SchoolContext")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ StudentDatabase/StudentDatabase/DAL/SchoolInitializer.cs
@@ -0,0 +1,27 @@
+﻿using StudentDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentDatabase.DAL
+{
+    public class SchoolInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<SchoolContext>
+    {
+        protected override void Seed(SchoolContext context)
+        {
+            var students = new List<Student>
+            {
+            new Student{FirstName="Carson",LastName="Alexander", },
+            new Student{FirstName="Meredith",LastName="Alonso", },
+            new Student{FirstName="Arturo",LastName="Anand", },
+            new Student{FirstName="Gytis",LastName="Barzdukas", },
+            new Student{FirstName="Yan",LastName="Li", },
+            new Student{FirstName="Peggy",LastName="Justice", },
+            new Student{FirstName="Laura",LastName="Norman", },
+            new Student{FirstName="Nino",LastName="Olivetto", },
+            };
+            context.SaveChanges();
+        }
+    }
+}
